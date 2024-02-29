fa2868be834dd47298bfcfff608cec5c90ac8b3a
Felipe <luis.londonogu@unaula.edu.co>
2024-02-28 22:33:45 -0500

    Se implementan los siguientes cambios:
    limpieza de carpetas

--- src/main/java/org/prueba_tecnica/perfil_usuario/interfaces/CrearListarUsuario.java
+++
@@ -1,9 +0,0 @@
-package org.prueba_tecnica.perfil_usuario.interfaces;
-
-import org.prueba_tecnica.perfil_usuario.modelos.Usuario;
-
-public interface CrearListarUsuario {
-     Usuario buscarUsuario(int id);
-
-     boolean agregarUsuario(Usuario usuario);
-}
